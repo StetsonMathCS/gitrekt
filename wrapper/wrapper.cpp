6a90744f3fb62d891cb14164b59ab474b6c73363
bpearson <bpearson@londo.stetson.edu>
2016-04-17 18:46:55 -0400

    Wrapper can access # of commits in a directory

--- wrapper/wrapper.cpp
+++ wrapper/wrapper.cpp
@@ -2,15 +2,99 @@
 #include "wrapper.h"
 using namespace std;
 
-//Initialize libgit2
+/*
+ * Initialize libgit2
+ */
 wrapper::wrapper()
 {
 	git_libgit2_init();
-	cout << "libgit2 initialized!" << endl;
 }
 
+/*
+ * Get the total number of commits in a repository,
+ * given a certain directory
+ */
+int wrapper::getCommitCount(const char* directory)
+{
+	git_repository *repository = NULL;
+	
+	//Repository does not exist; commit count is 0
+	if(git_repository_open(&repository, directory) != 0)
+	{
+		return 0;
+	}
+
+	//Walker for traversal
+	git_revwalk *walker;
+	git_revwalk_new(&walker, repository);
+	git_revwalk_sorting(walker, GIT_SORT_TOPOLOGICAL | GIT_SORT_TIME);
+	git_revwalk_push_head(walker);
+
+	int i = 0;
+	git_oid oid;
+
+	//Iterate through the commits
+	while(git_revwalk_next(&oid, walker) == 0)
+	{
+		git_commit *c;
+		git_commit_lookup(&c, repository, &oid);
+		i++;
+		git_commit_free(c);
+	}
+
+	//Free the repository, return the value
+	git_repository_free(repository);
+	return i;
+		
+}
+
+/**
+ * Get the total number of commits in a repository,
+ * given a directory and an author
+ */
+int wrapper::getCommitCount(const char* directory, string author)
+{
+	git_repository *repository = NULL;
+	
+	//Repository does not exist; commit count is 0
+	if(git_repository_open(&repository, directory) != 0)
+	{
+		return 0;
+	}
+
+	//Walker for traversal
+	git_revwalk *walker;
+	git_revwalk_new(&walker, repository);
+	git_revwalk_sorting(walker, GIT_SORT_TOPOLOGICAL | GIT_SORT_TIME);
+	git_revwalk_push_head(walker);
+
+	int i = 0;
+	git_oid oid;
+
+	//Iterate through the commits
+	while(git_revwalk_next(&oid, walker) == 0)
+	{
+		git_commit *c;
+		git_commit_lookup(&c, repository, &oid);
+		cout << git_commit_committer << endl;;
+		git_commit_free(c);
+	}	
+
+	//Free the repository, return the value
+	git_repository_free(repository);
+	return 0;
+}
+
+/*
+ * Main file purely used to compile the code
+ */
 int main()
 {
-	wrapper::wrapper();
+	wrapper *w = new wrapper();
+	const char *directory = "/home/bpearson/csci221/final-project/gitrekt";
+	string author = "bpearson";
+	cout << w->getCommitCount(directory, author);
+	delete w;
+	delete directory;
 	return 0;
 }
--- wrapper/wrapper.h
+++ wrapper/wrapper.h
@@ -2,13 +2,21 @@
 #define WRAPPER_H
 
 #include <git2.h>
+using namespace std;
 
 class wrapper
 {
 	private:
 
 	public:
+		//Initialize libgit2
 		wrapper();
+
+		//Get the total number of commits
+		int getCommitCount(const char*);
+		
+		//Get the total number of commits by a certain author
+		int getCommitCount(const char*, string);
 };
 
 #endif
