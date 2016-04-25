af1e6341f870d58ea82ab6a0eab8ef0e69bcdabc
Andrew Sheridan <asherida@stetson.edu>
2016-04-24 23:45:48 -0400

    Completted output of html fles now just need to split and add neccessary varibales from constructor info

--- Report_Generation/report_generation.cpp
+++ Report_Generation/report_generation.cpp
@@ -28,7 +28,9 @@ Report_Generation::Report_Generation(string userName, int numberOfCommit, int li
 }
 
 
-
+/**
+** Area Chart Output to areachart.html
+**/
 void areaChart() {
 	ofstream outputFile("areachart.html");
 	//outputFile.open("index.html");
@@ -47,6 +49,9 @@ void areaChart() {
 
 }
 
+/**
+** Bar Graph Output to bargraph.html
+**/
 void barGraph() {
 	ofstream outputFile("bargraph.html");
 	//outputFile.open("index.html");
@@ -67,11 +72,14 @@ void barGraph() {
 
 }
 
-
+/**
+** line graph Output to linegraph.html
+**/
 void lineGraph() {
 	ofstream outputFile("linegraph.html");
 	//outputFile.open("index.html");
-	string output = "<HTML>\n <HEAD>\n <title>Test HTML PAGE</title></head>\n<body>\n <h1>Testing output HTML file</h1></body></HTML>";
+	string output = "<html>\n<head>\n<script type=\"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script>\n<script type=\"text/javascript\">\ngoogle.charts.load('current', {'packages':['corechart']});\ngoogle.charts.setOnLoadCallback(drawChart);\n// The commits appear at the bottom first number is line 1 second is line 2\nfunction drawChart() {\nvar data = google.visualization.arrayToDataTable([\n['Commit', 'Code', 'Comments'],\n['Commit1', 50, 20],\n['Commit2', 75, 35],\n['Commit3', 150, 40],\n['Commit4', 160, 60]\n]);\n\nvar options = {\ntitle: 'Title',\ncurveType: 'function',\nlegend: { position: 'bottom' }\n};\n\nvar chart = new google.visualization.LineChart(document.getElementById('curve_chart'));\n\nchart.draw(data, options);\n}\n</script>\n</head>\n<body>\n<div id=\"curve_chart\" style=\"width: 900px; height: 500px\"></div>\n</body>\n</html>";
+
 	outputFile << output;
 	
 	outputFile.close();
@@ -84,11 +92,14 @@ void lineGraph() {
 
 
 }
-
+/**
+** pie Chart Output to piechart.html
+**/
 void piechart() {
 	ofstream outputFile("piechart.html");
 	//outputFile.open("index.html");
-	string output = "<HTML>\n <HEAD>\n <title>Test HTML PAGE</title></head>\n<body>\n <h1>Testing output HTML file</h1></body></HTML>";
+	string output = "<html>\n<head>\n<script type=\"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script>\n<script type=\"text/javascript\">\ngoogle.charts.load('current', {'packages':['corechart']});\ngoogle.charts.setOnLoadCallback(drawChart);\nfunction drawChart() {\n	\nvar data = google.visualization.arrayToDataTable([\n['Users', 'Commits'],\n['User0', 11],\n['User1', 2],\n['User2', 2],\n['User3', 2],\n['User4', 7]\n]);\n\nvar options = {\n title: 'Title'\n};\n\nvar chart = new google.visualization.PieChart(document.getElementById('piechart'));\n\nchart.draw(data, options);\n}\n</script>\n</head>\n<body>\n<div id=\"piechart\" style=\"width: 900px; height: 500px;\"></div>\n</body>\n</html>";
+
 	outputFile << output;
 	
 	outputFile.close();
@@ -101,11 +112,14 @@ void piechart() {
 
 }
 
-
+/**
+** table Output to table.html
+**/
 void table() {
 		ofstream outputFile("table.html");
 	//outputFile.open("index.html");
-	string output = "<HTML>\n <HEAD>\n <title>Test HTML PAGE</title></head>\n<body>\n <h1>Testing output HTML file</h1></body></HTML>";
+	string output = "<html>\n<head>\n<script type=\"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script>\n<script type=\"text/javascript\">\ngoogle.charts.load('current', {'packages':['table']});\ngoogle.charts.setOnLoadCallback(drawTable);\n\nfunction drawTable() {\nvar data = new google.visualization.DataTable();\ndata.addColumn('string', 'User Name');\ndata.addColumn('number', 'Lines of Code');\ndata.addColumn('number', 'Percent of Code');\ndata.addRows([\n['User1',  150, {v: 55 , f: '55%'}],\n['User2',   5, {v: 2, f: '2%'}],\n['User3', 80, {v: 30, f: '30%'}],\n['User4',   40, {v: 15,  f: '15%'}]\n]);\n\nvar table = new google.visualization.Table(document.getElementById('table_div'));\n\ntable.draw(data, {showRowNumber: true, width: '100%', height: '100%'});\n}\n</script>\n</head>\n<body>\n<div id=\"table_div\"></div>\n</body>\n</html>";
+
 	outputFile << output;
 	
 	outputFile.close();
@@ -119,8 +133,12 @@ void table() {
 
 }
 
+
+/**
+** Tester Method
+**/
 int main() {
-	cout << "executing" << endl;
+	cout << "executing output" << endl;
 	areaChart();
 	barGraph();
 	lineGraph();
